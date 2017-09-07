59ed49fdb5714fcd7de802a80a71d4ff62ac9adb
Eric Petroelje <eric.petroelje@cqlcorp.com>
2017-09-06 20:20:09 -0400

    Stripped down ASP.NET MVC project.

---
+++ HotLunch/HotLunch.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HotLunch.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").IncludeDirectory("~/Content/js", "*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/css").IncludeDirectory("~/Content/css", "*.css"));
+        }
+    }
+}
---
+++ HotLunch/HotLunch.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotLunch.Web.MvcApplication" Language="C#" %>
